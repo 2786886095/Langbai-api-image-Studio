caa226bcfd780e207281cf6a596b68293cf895fc
Langbai <lb2710137168@gmail.com>
2026-08-11 21:28:19 +0800

    release: prepare v1.6.33

--- lib/gemini_web_gateway.dart
+++ lib/gemini_web_gateway.dart
@@ -815,7 +815,7 @@ class GeminiWebGatewayManager {
         await _json(response, 200, <String, Object?>{
           'status': 'ok',
           'provider': 'gemini_web',
-          'version': '1.6.32',
+          'version': '1.6.33',
           'protocol_version': '1',
           'companion_connected': companionConnected,
           'session_available': browserConnected && activeAccount.loginReady,
